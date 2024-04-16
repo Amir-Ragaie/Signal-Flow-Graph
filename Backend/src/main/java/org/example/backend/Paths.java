15cd78c13150b222097e601cdee8c7548df72cff
Karene Antoine Nassif <antoinekarene@gmail.com>
2024-04-16 16:23:52 +0200

    Paths+Gain+main to test

--- Backend/src/main/java/org/example/backend/Paths.java
+++ Backend/src/main/java/org/example/backend/Paths.java
@@ -4,10 +4,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Paths {
-    public static List<List<Integer>> findForwardPaths(int[][] graph) {
-        List<List<Integer>> allPaths = new ArrayList<>();
-        dfs(0, new ArrayList<>(), graph, allPaths);
-        return allPaths;
+    private  List<List<Integer>> forwardPaths = new ArrayList<>();
+    private List<Integer> gainOfForwardPaths = new ArrayList<>();
+    public List<List<Integer>> getForwardPaths() {
+        return forwardPaths;
+    }
+    public void setForwardPaths(List<List<Integer>> forwardPaths) {
+        this.forwardPaths = forwardPaths;
+    }
+    public List<Integer> getGainOfForwardPaths() {
+        return gainOfForwardPaths;
+    }
+    public void setGainOfForwardPaths(List<Integer> gainOfForwardPaths) {
+        this.gainOfForwardPaths = gainOfForwardPaths;
+    }
+
+    public void findForwardPaths(int[][] graph) {
+        //List<List<Integer>> allPaths = new ArrayList<>();
+        dfs(0, new ArrayList<>(), graph, forwardPaths);
     }
 
     private static void dfs(int node, List<Integer> path, int[][] graph, List<List<Integer>> allPaths) {
@@ -26,6 +40,16 @@ public class Paths {
         path.remove(path.size() - 1);
     }
 
+    public void findGainOfForwardPaths(int[][] graph, List<List<Integer>> forwardPaths) {
+        for (List<Integer> path : forwardPaths) {
+            int gain = 1;
+            for (int i = 0; i < path.size() - 1; i++) {
+                gain *= graph[path.get(i)][path.get(i + 1)];
+            }
+            gainOfForwardPaths.add(gain);
+        }
+    }
+
 //    public static void main(String[] args) {
 //        int[][] graphMatrix = {
 //                {0, 10, 5, 0, 0},
@@ -34,11 +58,12 @@ public class Paths {
 //                {0, 0, 0, 0, 20},
 //                {0, 0, 0, 0, 0}
 //        };
-//
-//        List<List<Integer>> forwardPaths = findForwardPaths(graphMatrix);
+//        forwardPaths = findForwardPaths(graphMatrix);
+//        gainOfForwardPaths = findGainOfForwardPaths(graphMatrix, forwardPaths);
 //        System.out.println("Forward paths from first node to last node:");
 //        for (List<Integer> path : forwardPaths) {
 //            System.out.println(path);
+//            System.out.println("Gain: " + gainOfForwardPaths.get(forwardPaths.indexOf(path)));
 //        }
 //    }
 }
